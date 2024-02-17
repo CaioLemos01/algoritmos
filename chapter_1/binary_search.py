ec201a3ccebc9717043f58c90b484c739bc6335b
CaioLemosP <caiolemos992@gmail.com>
2024-02-17 09:43:39 -0300

    Initial commit

---
+++ chapter_1/binary_search.py
@@ -0,0 +1,18 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def binarySearch(list, object):
+    lower = 0
+    higher = len(list) - 1
+
+    while(lower <= higher):
+        mid = (higher + lower) // 2
+        guess = list[mid]
+        if(guess == object):
+            return mid
+        elif(guess < object):
+            lower = mid + 1
+        else:
+            higher = mid - 1
+    return None
+
+print(binarySearch(lista, 3))
